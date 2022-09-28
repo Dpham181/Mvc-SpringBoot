071e6f5ccde1630ba5e8bc2d1aff26d71e06e7cc
danhpham <danhpham312@gmail.com>
2022-09-28 16:57:35 -0700

    Update AssignmentApplication.java

--- src/main/java/com/stringMvc/Assignment/AssignmentApplication.java
+++ src/main/java/com/stringMvc/Assignment/AssignmentApplication.java
@@ -2,9 +2,9 @@ package com.stringMvc.Assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class AssignmentApplication {
+public class AssignmentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentApplication.class, args);
